b25300f4a670b807cd5c28854c422047a499fa96
Radishdaluobo <191245518@qq.com>
2017-06-27 23:42:31 +0800

    add annotation

--- app/reducers/store.js
+++ app/reducers/store.js
@@ -10,6 +10,8 @@ export default function userinfo(state = initialState, action) {
             state.unshift(action.data)
                 //相当于手动写的add方法
             return state
+                //concat 是返回连接之后的新的数组，但是不会修改原始数组，
+                //unshift 返回的是被修改后的数组的长度,会修改原数组
         case actionTypes.STORE_RM:
             return state.filter(item => {
                 //相当于手动写的remove方法
