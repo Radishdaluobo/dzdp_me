42e5812e5043660213f927a04e799434fbd14d04
Radishdaluobo <191245518@qq.com>
2017-07-08 11:02:53 +0800

    add commit

--- app/components/OrderList/Item/index.jsx
+++ app/components/OrderList/Item/index.jsx
@@ -11,8 +11,20 @@ class Item extends React.Component {
     render() {
         const data = this.props.data;
         return (
-            <div className="item clear-fix">
-                item detail
+            <div className="item clear-fix">             
+                    <div className="item-img f-l">
+                        <img src={data.img} alt=""/>
+                    </div>
+                    <div className="item-infomation f-l">
+                        <ul>
+                            <li>商户: <span>{data.title}</span></li>
+                            <li>数量: <span>{data.count}</span></li>
+                            <li>价格: <span>{data.price}</span></li>
+                        </ul>
+                    </div>
+                    <div className="item-commit f-r">
+                        <button>评价</button>
+                    </div>
             </div>
         )
     }
